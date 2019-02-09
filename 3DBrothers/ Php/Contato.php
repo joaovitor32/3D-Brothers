b00bc23f0308499b5367999e7df04a6cb598ada0
Bozinao <joaovitormunizlopes@gmail.com>
2019-02-09 11:50:56 -0200

    Update and rename 3DBrothers/ Php/Contato.php to  3DBrothers/ Php/Contato.php

--- 3DBrothers/ Php/Contato.php
+++ 3DBrothers/ Php/Contato.php
@@ -5,12 +5,9 @@
 	$Email=$_POST['Email'];
 	$Telefone=$_POST['Telefone'];
 	$Mensagem=$_POST['Mensagem'];
-
-<<<<<<< HEAD
 	$SQLInsert="INSERT INTO Ocorrencia (Nome, Email, Telefone, Mensagem) VALUES('".$Nome."','".$Email."','".$Telefone."','".$Mensagem."')";		
 	mysqli_query($Conexao, $SQLInsert);
 	mysqli_close($Conexao);
-
 	$Redirecionar="/3D-Brothers/3DBrothers/";
 	header("location:$Redirecionar");
 ?>

2374366709c02e0d20ccfad7b3d0bf0753d4b053
Bozina <joaovitormunizlopes@gmail.com>
2019-02-10 08:34:50 -0200

    Encapsulamento da conexao

--- 3DBrothers/Php/Conexao.php
+++ 3DBrothers/Php/Conexao.php
@@ -1,7 +1,20 @@
 <?php
-    $host = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "Mensagem1";
-    $Conexao =mysqli_connect($host, $username, $password, $dbname);    
+    class BancoDeDados{
+
+        private $host = "127.0.0.1";
+        private $username = "root";
+        private $password = "";
+        private $dbname = "Mensagem1";
+    
+        public function ConectaBanco(){
+            $Conexao=mysqli_connect($this->host,$this->username,$this->password,$this->dbname);
+            mysqli_set_charset($Conexao,'utf-8');
+            if(mysqli_connect_errno()){
+                echo 'Erro ao conectar ao banco'.mysqli_connect_errno();
+            }else{
+                return $Conexao;
+            }
+        }
+    }    
 ?>
+

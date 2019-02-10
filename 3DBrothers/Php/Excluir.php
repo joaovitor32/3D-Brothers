17017fe0bad2827699c396f0820ab8bd3c1fa365
Bozina <joaovitormunizlopes@gmail.com>
2019-02-10 09:42:08 -0200

    Remocao de arquivos duplicados

--- 3DBrothers/Php/Excluir.php
+++
@@ -1,7 +0,0 @@
- <?php 
-   session_start();
-   $BD=new BancoDeDados();
-   $SQLDelete="DELETE FROM Ocorrencia";
-   mysqli_query($BD->ConectarBanco(),$SQLDelete) or die(mysqli_error($Conexao));
-   mysqli_close($BD->ConectarBanco());
-?>
--- 3DBrothers/Php/Operacoes.php
+++
@@ -1,6 +0,0 @@
- <?php 
-  $Codigo=0;
-   $SQLDelete="DELETE FROM Ocorrencia WHERE CodOcorrencia=".$Codigo."";
-   mysqli_query($Conexao,$SQLDelete) or die(mysqli_error($Conexao));
-   mysqli_close($Conexao);
-?>
